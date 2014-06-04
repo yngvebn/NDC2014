c3b7c0296e56c962fbb314bef260ead50278bbe2
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2014-06-04 21:29:41 +0200

    Ported Front-page

---
+++ Demo/Demo/public/movies/FrontPageMovieList/MovieList.js
@@ -0,0 +1,18 @@
+﻿angular.module('movies').controller('MovieList', [
+    '$scope', '$http',
+    function ($scope, $http) {
+        $scope.movies = [];
+        $scope.genres = [];
+        $scope.selectedGenre = '';
+
+        $scope.toggleGenre = function (genre) {
+            if ($scope.selectedGenre === genre) genre = '';
+            $scope.selectedGenre = genre;
+        }
+
+        $http.get('/api/v1/movies').success(function (result) {
+            $scope.movies = result;
+            $scope.genres = _.chain($scope.movies).flatten('genres').uniq().value();
+        });
+    }
+]);
